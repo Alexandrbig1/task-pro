dddb2ddd3ed5985ec26e71d1c0259ff5a7a3bf65
Alexandrbig1 <alexsmagin1@gmail.com>
2024-01-31 13:58:48 -0600

    feat: created new project. added header component

---
+++ src/components/Header/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import { HeaderWrapper } from "./Header.styled";
+
+export default function Header() {
+  return (
+    <HeaderWrapper>
+      <p>logo</p>
+      <div>
+        <p>theme</p>
+        <div>
+          <p>John</p>
+          <span>img</span>
+        </div>
+      </div>
+    </HeaderWrapper>
+  );
+}
