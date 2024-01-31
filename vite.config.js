767f414148f73decfd796ee61062de0db0a22839
Alexandrbig1 <alexsmagin1@gmail.com>
2024-01-31 17:50:11 -0600

    feat: added vite config. fix: updated package json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/personal-trainer/",
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "build",
+    assetsDir: "assets",
+  },
+  css: {
+    modules: false,
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
