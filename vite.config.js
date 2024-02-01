91bcb5a201d5ce4b35d9e4c322100de18dc4c097
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-01 01:03:03 -0600

    fix: updated path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/task-pro/",
   plugins: [react()],
   server: {
     port: 3000,
