fe375f81414f546b7c9da7266a33436f0e58314d
batalova-kira <batalovakira@gmail.com>
2024-02-03 19:52:27 +0200

    create-edit-profile-modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,24 @@
 // import Logo from "../Logo/Logo";
 import MenuBurger from "../MenuBurger/MenuBurger";
 import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
-import { HeaderWrapper, ThemeSwitcherWrapper, UserData } from "./Header.styled";
+import UserData from "../UserData/UserData";
+import { HeaderWrapper, ThemeSwitcherWrapper } from "./Header.styled";
 import PropTypes from "prop-types";
 
 export default function Header({ toggleTheme, handleAsideHide }) {
-  return (
-    <HeaderWrapper>
-      <MenuBurger handleAsideHide={handleAsideHide} />
-      <ThemeSwitcherWrapper>
-        <ThemeSwitcher toggleTheme={toggleTheme} />
-        <UserData>
-          <p>John</p>
-          <span>img</span>
-        </UserData>
-      </ThemeSwitcherWrapper>
-    </HeaderWrapper>
-  );
+    return (
+        <HeaderWrapper>
+            <MenuBurger handleAsideHide={handleAsideHide} />
+            <ThemeSwitcherWrapper>
+                <ThemeSwitcher toggleTheme={toggleTheme} />
+
+                <UserData />
+            </ThemeSwitcherWrapper>
+        </HeaderWrapper>
+    );
 }
 
 Header.propTypes = {
-  toggleTheme: PropTypes.func,
-  handleAsideHide: PropTypes.func,
+    toggleTheme: PropTypes.func,
+    handleAsideHide: PropTypes.func,
 };
---
+++ src/components/UserData/UserData.styled.jsx
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+import { primaryFont } from "../fonts";
+
+export const AvatarWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+`;
+
+export const AvatarText = styled.p`
+    font-family: ${primaryFont};
+    font-weight: 500;
+    font-size: 1.4rem;
+    letter-spacing: -0.02em;
+    color: ${(p) => p.theme.colors.mainTextColor};
+`;
+export const AvatarIcon = styled.svg`
+    width: ${(p) => (p?.size?.width ? p?.size?.width : "32px")};
+    height: ${(p) => (p?.size?.height ? p?.size?.height : "32px")};
+`;
+
+export const AvatarBtn = styled.button`
+    border-radius: 8px;
+    border: none;
+
+    width: ${(p) => (p?.size?.width ? p?.size?.width : "32px")};
+    height: ${(p) => (p?.size?.height ? p?.size?.height : "32px")};
+`;
+
+export const AvatarModal = styled.div`
+    position: absolute;
+    max-width: 400px;
+    width: 100%;
+    min-height: 440px;
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    &:hover {
+        cursor: auto;
+    }
+    /* display: flex;
+    gap: 0.8rem;
+    color: ${(p) => p.theme.colors.mainTextColor};
+    background-color: ${(p) => p.theme.colors.whiteColor}; */
+`;
+
+export const StyledModal = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    &:hover {
+        cursor: pointer;
+    }
+
+    .btnClose {
+        position: absolute;
+        top: 14px;
+        right: 14px;
+    }
+    /* width: 100vw; //розтягуємо модалку на ширину та висоту екрана
+    height: 100vh; */
+`;
