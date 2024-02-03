e392601165d409a3eec5e031293b49058f836f8f
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-03 15:05:00 -0600

    fix: updated styled, colors on needhelp component

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,68 +1,68 @@
 import { useEffect, useState } from "react";
 import {
-    AvatarBtn,
-    AvatarIcon,
-    AvatarModal,
-    AvatarText,
-    AvatarWrapper,
-    StyledModal,
+  AvatarBtn,
+  AvatarIcon,
+  AvatarModal,
+  AvatarText,
+  AvatarWrapper,
+  StyledModal,
 } from "./UserData.styled";
 
 export default function UserData() {
-    const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-    // const openModal = () => {
-    //     setIsOpenModal(true);
-    // };
+  // const openModal = () => {
+  //     setIsOpenModal(true);
+  // };
 
-    // const closeModal = () => {
-    //     setIsOpenModal(false);
-    // };
+  // const closeModal = () => {
+  //     setIsOpenModal(false);
+  // };
 
-    function toggleModal() {
-        setIsOpenModal((prevState) => !prevState);
+  function toggleModal() {
+    setIsOpenModal((prevState) => !prevState);
+  }
+  //   useEffect(() => {
+  //     const handleKeyDown = (e) => {
+  //       if (e.code === "Escape") {
+  //         toggleModal();
+  //       }
+  //     };
+
+  //     window.addEventListener("keydown", handleKeyDown);
+  //     document.body.style.overflow = "hidden";
+  //     return () => {
+  //       window.removeEventListener("keydown", handleKeyDown);
+  //       document.body.style.overflow = "auto";
+  //     };
+  //   }, []);
+
+  const handleOverlayClick = (e) => {
+    if (e.currentTarget === e.target) {
+      toggleModal();
     }
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            if (e.code === "Escape") {
-                toggleModal();
-            }
-        };
-
-        window.addEventListener("keydown", handleKeyDown);
-        document.body.style.overflow = "hidden";
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-            document.body.style.overflow = "auto";
-        };
-    }, []);
-
-    const handleOverlayClick = (e) => {
-        if (e.currentTarget === e.target) {
-            toggleModal();
-        }
-    };
-    return (
-        <>
-            <AvatarWrapper onClick={handleOverlayClick}>
-                <AvatarText>John</AvatarText>
-                <AvatarBtn onClick={() => toggleModal()}>
-                    <AvatarIcon>
-                        <use href="images/icons.svg#icon-userAvatarDefault"></use>
-                    </AvatarIcon>
-                </AvatarBtn>
-            </AvatarWrapper>
-            {isOpenModal && (
-                <StyledModal onClick={handleOverlayClick}>
-                    <AvatarModal>
-                        <button onClick={toggleModal} className="btnClose">
-                            &times;
-                        </button>
-                        <h1>333333</h1>
-                        <p>Hello</p>
-                    </AvatarModal>
-                </StyledModal>
-            )}
-        </>
-    );
+  };
+  return (
+    <>
+      <AvatarWrapper onClick={handleOverlayClick}>
+        <AvatarText>John</AvatarText>
+        <AvatarBtn onClick={() => toggleModal()}>
+          <AvatarIcon>
+            <use href="images/icons.svg#icon-userAvatarDefault"></use>
+          </AvatarIcon>
+        </AvatarBtn>
+      </AvatarWrapper>
+      {isOpenModal && (
+        <StyledModal onClick={handleOverlayClick}>
+          <AvatarModal>
+            <button onClick={toggleModal} className="btnClose">
+              &times;
+            </button>
+            <h1>333333</h1>
+            <p>Hello</p>
+          </AvatarModal>
+        </StyledModal>
+      )}
+    </>
+  );
 }
