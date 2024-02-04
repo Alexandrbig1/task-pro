fa105876de5ad602a9b24a4529aa99cc6eb60a99
batalova-kira <batalovakira@gmail.com>
2024-02-04 21:17:14 +0200

    fix colors

--- src/components/EditProfileForm/EditProfileForm.styled.jsx
+++ src/components/EditProfileForm/EditProfileForm.styled.jsx
@@ -2,70 +2,71 @@ import styled from "styled-components";
 // import { primaryFont } from "../fonts";
 import { FiPlus } from "react-icons/fi";
 export const StyledForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 14px;
-  margin-bottom: 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 14px;
+    margin-bottom: 24px;
 `;
 
 export const WrapperUpdateAvatar = styled.div`
-  width: 6.8rem;
-  height: 6.8rem;
-  border: "1px solid blue";
+    width: 6.8rem;
+    height: 6.8rem;
+    border: "1px solid blue";
 `;
 
 export const UpdateAvatar = styled.img`
-  width: 6.8rem;
-  height: 6.8rem;
-  outline: none;
-  border: none;
-  border-radius: 0.8rem;
-  border: "1px solid blue";
+    width: 6.8rem;
+    height: 6.8rem;
+    outline: none;
+    border: none;
+    border-radius: 0.8rem;
+    border: "1px solid blue";
 `;
 export const LabelAvatar = styled.label`
-  position: relative;
+    position: relative;
 
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 `;
 
 export const BtnSubmit = styled.button`
-  display: block;
-  border-radius: 0.8rem;
-  width: 28.7rem;
-  height: 4.9rem;
-  border: none;
-  outline: none;
-  background-color: ${(p) => p.theme.colors.accentBgColor};
+    display: block;
+    border-radius: 0.8rem;
+    width: 28.7rem;
+    height: 4.9rem;
+    border: none;
+    outline: none;
+    background-color: ${(p) => p.theme.colors.themeTextHoverColor};
 `;
 
 export const IconPlus = styled.svg`
-  width: ${(p) => (p?.size?.width ? p?.size?.width : "10px")};
-  height: ${(p) => (p?.size?.height ? p?.size?.height : "10px")};
+    font-size: 1rem;
+    /* width: ${(p) => (p?.size?.width ? p?.size?.width : "10px")};
+    height: ${(p) => (p?.size?.height ? p?.size?.height : "10px")}; */
 `;
 
 export const BtnWrapper = styled.div`
-  background-color: ${(p) => p.theme.colors.accentBgColor};
-  padding: 7px;
-  border-radius: 8px;
-  position: absolute;
-  bottom: 0;
-  left: -50%;
+    background-color: ${(p) => p.theme.colors.buttonsBgColor};
+    padding: 7px;
+    border-radius: 8px;
+    position: absolute;
+    bottom: 0;
+    left: -50%;
 
-  transform: translateX(-50%);
+    transform: translateX(-50%);
 
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-  &:hover {
-    cursor: pointer;
-  }
+    &:hover {
+        cursor: pointer;
+    }
 `;
 
 export const BtnPlus = styled(FiPlus)`
-  color: ${(p) => p.theme.colors.mainTextColor};
-  font-size: 1.6rem;
+    color: ${(p) => p.theme.colors.mainTextColor};
+    font-size: 1.6rem;
 `;
