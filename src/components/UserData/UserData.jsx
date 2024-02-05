15cdd7fcefc5073bb7ce5d0af1860ea70ede340f
batalova-kira <batalovakira@gmail.com>
2024-02-05 10:24:21 +0200

    create-branch

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
-// import { useEffect, useState } from "react";
 import {
     AvatarBtn,
     AvatarIcon,
     AvatarText,
     AvatarWrapper,
-    // AvatarModal,
-    // StyledModal,
 } from "./UserData.styled";
 import EditProfileModal from "../EditProfileModal/EditProfileModal";
 
@@ -30,59 +27,4 @@ export default function UserData() {
             {isOpenModal && <EditProfileModal toggleModal={toggleModal} />}
         </>
     );
-
-    // const openModal = () => {
-    //     setIsOpenModal(true);
-    // };
-
-    // const closeModal = () => {
-    //     setIsOpenModal(false);
-    // };
-
-    // function toggleModal() {
-    //   setIsOpenModal((prevState) => !prevState);
-    // }
-    //   useEffect(() => {
-    //     const handleKeyDown = (e) => {
-    //       if (e.code === "Escape") {
-    //         toggleModal();
-    //       }
-    //     };
-
-    //     window.addEventListener("keydown", handleKeyDown);
-    //     document.body.style.overflow = "hidden";
-    //     return () => {
-    //       window.removeEventListener("keydown", handleKeyDown);
-    //       document.body.style.overflow = "auto";
-    //     };
-    //   }, []);
-
-    //   const handleOverlayClick = (e) => {
-    //     if (e.currentTarget === e.target) {
-    //       toggleModal();
-    //     }
-    //   };
-    //   return (
-    //     <>
-    //       <AvatarWrapper onClick={handleOverlayClick}>
-    //         <AvatarText>John</AvatarText>
-    //         <AvatarBtn onClick={() => toggleModal()}>
-    //           <AvatarIcon>
-    //             <use href="images/icons.svg#icon-userAvatarDefault"></use>
-    //           </AvatarIcon>
-    //         </AvatarBtn>
-    //       </AvatarWrapper>
-    //       {isOpenModal && (
-    //         <StyledModal onClick={handleOverlayClick}>
-    //           <AvatarModal>
-    //             <button onClick={toggleModal} className="btnClose">
-    //               &times;
-    //             </button>
-    //             <h1>333333</h1>
-    //             <p>Hello</p>
-    //           </AvatarModal>
-    //         </StyledModal>
-    //       )}
-    //     </>
-    //   );
 }
