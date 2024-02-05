e9e666d96ddf580b7f08663d729f040fb9fb6885
Yullia90 <y.tsarenko@harwind.com.ua>
2024-02-05 22:38:39 +0200

    add edit-column modal

---
+++ src/components/EditColumnModal/EditColumnModal.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react";
+import { PropTypes } from "prop-types";
+import {
+  AddModalWrap,
+  StyledAddModal,
+  AddColumnModalBtn,
+  CloseAddColumnModal,
+  AddColumnTitle,
+  AddColumnForm,
+  AddColumnInput,
+} from "../EditColumnModal/EditColumnModal.styled";
+import { CardButton } from "../CardButton/CardButton";
+
+const EditColumnModal = ({ openEditColumnModal }) => {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Escape") {
+        openEditColumnModal();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      document.body.style.overflow = "auto";
+    };
+  }, [openEditColumnModal]);
+
+  const handleEditModalClick = (e) => {
+    if (e.target === e.currentTarget) {
+      openEditColumnModal();
+    }
+  };
+  return (
+    <EditModalWrap onClick={handleEditModalClick}>
+      <StyledEditModal class="modal">
+        <EditColumnModalBtn onclick={openEditColumnModal} type="button">
+          <CloseEditColumnModal />
+        </EditColumnModalBtn>
+        <div>
+          <EditColumnTitle>Edit column</EditColumnTitle>
+          <EditColumnForm>
+            <EditColumnInput type="text" placeholder="To Do" />
+            <CardButton btnText="Add" />
+          </EditColumnForm>
+        </div>
+      </StyledEditModal>
+    </EditModalWrap>
+  );
+};
+
+export default EditColumnModal;
+
+EditColumnModal.propTypes = {
+  openEditColumnModal: PropTypes.func,
+};
