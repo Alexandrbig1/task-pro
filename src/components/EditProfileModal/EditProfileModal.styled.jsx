28d849f1290936cfa675ebf8732b5425f8794428
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-05 14:18:35 -0600

    fix: updated styles

--- src/components/EditProfileModal/EditProfileModal.styled.jsx
+++ src/components/EditProfileModal/EditProfileModal.styled.jsx
@@ -3,66 +3,67 @@ import { primaryFont } from "../fonts";
 import { IoClose } from "react-icons/io5";
 
 export const StyledModal = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    background-color: rgba(0, 0, 0, 0.5);
-    &:hover {
-        cursor: pointer;
-    }
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const AvatarModal = styled.div`
-    position: absolute;
-    width: 40rem;
-    height: 44rem;
-    background-color: ${(p) => p.theme.colors.mainHeaderColor};
-    padding: 2.4rem;
-    border-radius: 1rem;
+  position: absolute;
+  width: 40rem;
+  /* height: 44rem; */
+  background-color: ${(p) => p.theme.colors.mainHeaderColor};
+  padding: 2.4rem;
+  border-radius: 1rem;
 
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    &:hover {
-        cursor: auto;
-    }
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  &:hover {
+    cursor: auto;
+  }
 
-    @media (max-width: 767px) {
-        max-width: 33.5rem;
-        width: 100%;
-    }
+  @media (max-width: 767px) {
+    max-width: 33.5rem;
+    width: 100%;
+  }
 `;
 
 export const BtnClose = styled(IoClose)`
-    fill: ${(p) => p.theme.colors.burgerColor};
+  fill: ${(p) => p.theme.colors.burgerColor};
 
-    position: absolute;
-    top: 1.4rem;
-    right: 1.4rem;
+  position: absolute;
+  top: 1.4rem;
+  right: 1.4rem;
 
-    width: 1.8rem;
-    height: 1.8rem;
-    background-color: transparent;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    transition: 0.3s linear;
+  width: 1.8rem;
+  height: 1.8rem;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: 0.3s linear;
 
-    &:hover {
-        transform: rotate(90deg);
-        fill: ${(p) => p.theme.colors.accentTextColor};
-    }
+  &:hover {
+    transform: rotate(90deg);
+    fill: ${(p) => p.theme.colors.accentTextColor};
+  }
 `;
 
 export const TitleModalText = styled.h2`
-    padding-top: 3px;
-    padding-bottom: 3px;
-    margin-bottom: 2.4rem;
-    font-family: ${primaryFont};
-    font-weight: 500;
-    font-size: 1.8rem;
-    letter-spacing: -0.02em;
-    color: ${(p) => p.theme.colors.secondaryTextColor};
+  padding-top: 3px;
+  padding-bottom: 3px;
+  margin-bottom: 2.4rem;
+  font-family: ${primaryFont};
+  font-weight: 500;
+  font-size: 1.8rem;
+  letter-spacing: -0.02em;
+  color: ${(p) => p.theme.colors.secondaryTextColor};
 `;
--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,28 +1,28 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1.8rem 3.2rem;
-    height: 6rem;
-    width: 100%;
-    background-color: ${(p) => p.theme.colors.mainHeaderColor};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.8rem 3.2rem;
+  height: 6rem;
+  width: 100%;
+  background-color: ${(p) => p.theme.colors.mainHeaderColor};
 
-    @media (min-width: 768px) {
-        height: 6.8rem;
-    }
+  @media (min-width: 768px) {
+    height: 6.8rem;
+  }
 
-    @media (min-width: 1440px) {
-        justify-content: end;
-        padding: 1.8rem 2.4rem;
-    }
+  @media (min-width: 1440px) {
+    justify-content: end;
+    padding: 1.8rem 2.4rem;
+  }
 `;
 
 export const ThemeSwitcherWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 1.4rem;
+  display: flex;
+  align-items: center;
+  gap: 1.4rem;
 `;
 
 // export const UserData = styled.div`
