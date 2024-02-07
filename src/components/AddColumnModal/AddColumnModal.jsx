b818c1fbd39d5e0c9d0109f7b953e5e36bb03c37
Yullia90 <y.tsarenko@harwind.com.ua>
2024-02-08 01:16:27 +0200

    add overlay

--- src/components/AddColumnModal/AddColumnModal.jsx
+++ src/components/AddColumnModal/AddColumnModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import  PropTypes  from "prop-types";
 import {
   AddModalWrap,
@@ -10,6 +10,10 @@ import {
   AddColumnInput,
 } from "../AddColumnModal/AddColumnModal.styled";
 import { CardButton } from "../CardButton/CardButton";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
 
 const AddColumnModal = ({ openColumnModal }) => {
     useEffect(() => {
@@ -32,7 +36,43 @@ const AddColumnModal = ({ openColumnModal }) => {
         if (e.target === e.currentTarget) {
             openColumnModal();
         }
-    };
+  };
+  
+  const onSubmitColumnClick = async (e) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const title = form.elements.title.value;
+
+    const validTitle = title.length > 0;
+
+    if (validTitle) {
+      form.reset();
+      openColumnModal();
+      toast.success("You are successful add column!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      });
+      } else {
+      toast.error("Please enter a title", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: false,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }
+
     return (
       <AddModalWrap onClick={handleModalClick}>
         <StyledAddModal className="modal">
@@ -41,7 +81,7 @@ const AddColumnModal = ({ openColumnModal }) => {
           </AddColumnModalBtn>
           <div>
             <AddColumnTitle>Add column</AddColumnTitle>
-            <AddColumnForm>
+            <AddColumnForm onSubmit={onSubmitColumnClick}>
               <AddColumnInput type="text" placeholder="Title" />
               <CardButton btnText="Add" />
             </AddColumnForm>
@@ -57,6 +97,83 @@ AddColumnModal.propTypes = {
   openColumnModal: PropTypes.func,
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useEffect } from "react";
+// import  PropTypes  from "prop-types";
+// import {
+//   AddModalWrap,
+//   StyledAddModal,
+//   AddColumnModalBtn,
+//   CloseAddColumnModal,
+//   AddColumnTitle,
+//   AddColumnForm,
+//   AddColumnInput,
+// } from "../AddColumnModal/AddColumnModal.styled";
+// import { CardButton } from "../CardButton/CardButton";
+
+// const AddColumnModal = ({ openColumnModal }) => {
+//     useEffect(() => {
+//         const handleKeyDown = (e) => {
+//             if (e.code === "Escape") {
+//                 openColumnModal();
+//             }
+//         };
+
+//         window.addEventListener("keydown", handleKeyDown);
+//         document.body.style.overflow = "hidden";
+
+//         return () => {
+//             window.removeEventListener("keydown", handleKeyDown);
+//             document.body.style.overflow = "auto";
+//         };
+//     }, [openColumnModal]);
+
+//     const handleModalClick = (e) => {
+//         if (e.target === e.currentTarget) {
+//             openColumnModal();
+//         }
+//     };
+//     return (
+//       <AddModalWrap onClick={handleModalClick}>
+//         <StyledAddModal className="modal">
+//           <AddColumnModalBtn onClick={openColumnModal} type="button">
+//             <CloseAddColumnModal />
+//           </AddColumnModalBtn>
+//           <div>
+//             <AddColumnTitle>Add column</AddColumnTitle>
+//             <AddColumnForm>
+//               <AddColumnInput type="text" placeholder="Title" />
+//               <CardButton btnText="Add" />
+//             </AddColumnForm>
+//           </div>
+//         </StyledAddModal>
+//       </AddModalWrap>
+//     );
+// };
+
+// export default AddColumnModal;
+
+// AddColumnModal.propTypes = {
+//   openColumnModal: PropTypes.func,
+// };
 
 //Connect
 
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,4 +1,51 @@
+// import Logo from "../../components/Logo/Logo";
+// import {
+//   WelcomeWrapper,
+//   WelcomeContainer,
+//   WelcomeText,
+//   WelcomeBtnWrapper,
+//   RegistrationBtn,
+//   LoginBtn,
+// } from "./Welcome.styled";
+
+// const size = {
+//   width: "48px",
+//   height: "48px",
+// };
+
+// const text = {
+//   color: "#161616",
+//   font: "4rem",
+// };
+
+// const icon = {
+//   bgColor: "#161616",
+//   iconColor: "#fff",
+// };
+
+// export default function Welcome() {
+//   return (
+//     <WelcomeContainer>
+//       <WelcomeWrapper>
+//         <img src="images/welcome.png" alt="boy with laptop" />
+//         <Logo size={size} text={text} icon={icon} />
+//         <WelcomeText>
+//           Supercharge your productivity and take control of your tasks with Task
+//           Pro - Don't wait, start achieving your goals now!
+//         </WelcomeText>
+//         <WelcomeBtnWrapper>
+//           <RegistrationBtn to="auth/signup">Registration</RegistrationBtn>
+//           <LoginBtn to="auth/signin">Log In</LoginBtn>
+//         </WelcomeBtnWrapper>
+//       </WelcomeWrapper>
+//     </WelcomeContainer>
+//   );
+// }
+
+
+
 import Logo from "../../components/Logo/Logo";
+import { useState } from "react";
 import {
   WelcomeWrapper,
   WelcomeContainer,
@@ -7,6 +54,7 @@ import {
   RegistrationBtn,
   LoginBtn,
 } from "./Welcome.styled";
+import AddColumnModal from "../../components/AddColumnModal/AddColumnModal";
 
 const size = {
   width: "48px",
@@ -22,8 +70,15 @@ const icon = {
   bgColor: "#161616",
   iconColor: "#fff",
 };
+const Welcome = () => {
+  // Стан для управління видимістю модального вікна
+  const [isModalColumnOpen, setIsModalColumnOpen] = useState(false);
 
-export default function Welcome() {
+  //   // Функція для відкриття та закриття модального вікна
+   const openColumnModal = () => {
+     setIsModalColumnOpen((prevState) => !prevState);
+   };
+
   return (
     <WelcomeContainer>
       <WelcomeWrapper>
@@ -36,8 +91,15 @@ export default function Welcome() {
         <WelcomeBtnWrapper>
           <RegistrationBtn to="auth/signup">Registration</RegistrationBtn>
           <LoginBtn to="auth/signin">Log In</LoginBtn>
+          <LoginBtn onClick={() => openColumnModal()}>
+            Add another column
+          </LoginBtn>
+          {isModalColumnOpen && (
+            <AddColumnModal openColumnModal={openColumnModal} />
+          )}
         </WelcomeBtnWrapper>
       </WelcomeWrapper>
     </WelcomeContainer>
   );
-}
+};
+export default Welcome;
