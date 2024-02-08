369243ef6657f0a10b6c933568830b800b3daeef
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-08 06:37:18 -0600

    Updates

--- assets/Logo-S0S3Ts2r.js
+++ assets/Logo-S0S3Ts2r.js
@@ -1,4 +1,4 @@
-import{u as n,p as s,j as i}from"./index-PETCG1QS.js";const l=n.a`
+import{u as n,p as s,j as i}from"./index-0G1yFJre.js";const l=n.a`
   color: ${o=>o.theme.colors.mainTextColor};
   font-family: ${s};
   font-size: 1.6rem;
---
+++ assets/Welcome-FOq-oU7x.js
@@ -0,0 +1,113 @@
+import{u as e,p as n,L as o,j as t,H as r,a}from"./index-0G1yFJre.js";import{L as l}from"./Logo-S0S3Ts2r.js";import{G as c}from"./iconBase-9iaLz2w_.js";function s(i){return c({tag:"svg",attr:{version:"1.1",x:"0px",y:"0px",viewBox:"0 0 48 48",enableBackground:"new 0 0 48 48"},child:[{tag:"path",attr:{fill:"#FFC107",d:`M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12\r
+	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24\r
+	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z`},child:[]},{tag:"path",attr:{fill:"#FF3D00",d:`M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657\r
+	C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z`},child:[]},{tag:"path",attr:{fill:"#4CAF50",d:`M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36\r
+	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z`},child:[]},{tag:"path",attr:{fill:"#1976D2",d:`M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571\r
+	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z`},child:[]}]})(i)}const d=e.div`
+  background: linear-gradient(
+    180deg,
+    rgba(196, 196, 196, 0) 25%,
+    ${i=>i.theme.colors.accentBgColor} 92.19%
+  );
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+  margin: 0 auto;
+  padding: 0 2rem;
+
+  /* @media (min-width: 375px) {
+    max-width: 37.5rem;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 76.8rem;
+  }
+
+  @media (min-width: 1440px) {
+    max-width: 144rem;
+  } */
+`,m=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2.4rem;
+`,h=e.p`
+  color: #161616;
+  text-align: center;
+  font-family: ${n};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.28;
+  letter-spacing: -0.28px;
+  max-width: 300px;
+  width: 100%;
+
+  @media (min-width: 375px) {
+    max-width: 335px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 474px;
+  }
+`,p=e.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.4rem;
+`,x=e(o)`
+  color: #fff;
+  text-align: center;
+  font-family: ${n};
+  font-size: 1.4rem;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.28px;
+
+  border-radius: 8px;
+  background-color: #161616;
+
+  display: flex;
+  width: 300px;
+  padding: 14px 0px;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  outline: none;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  @media (min-width: 375px) {
+    max-width: 335px;
+  }
+
+  @media (min-width: 768px) {
+    width: 344px;
+  }
+`,g=e(o)`
+  color: #161616;
+  font-family: ${n};
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.28px;
+  text-align: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`,f=e(s)`
+  font-size: 2.4rem;
+
+  &:hover {
+    cursor: pointer;
+  }
+`,u={width:"48px",height:"48px"},w={color:"#161616",font:"4rem"},y={bgColor:"#161616",iconColor:"#fff"};function k(){function i(){console.log("google")}return t.jsxs(t.Fragment,{children:[t.jsx(r,{children:t.jsxs(a,{children:[t.jsx("title",{children:"Creamy Sharks - Sign Up"}),t.jsx("meta",{name:"description",content:"Explore TaskPro, the ultimate task manager born out of a 10-month Fullstack Development Bootcamp. Elevate your productivity with TaskPro's intuitive design and powerful functionality."})]})}),t.jsx(d,{children:t.jsxs(m,{children:[t.jsx("img",{src:"images/welcome.png",alt:"boy with laptop"}),t.jsx(l,{size:u,text:w,icon:y}),t.jsx(h,{children:"Supercharge your productivity and take control of your tasks with Task Pro - Don't wait, start achieving your goals now!"}),t.jsxs(p,{children:[t.jsx(x,{to:"signup",children:"Registration"}),t.jsx(g,{to:"signin",children:"Log In"}),t.jsx(f,{onClick:i})]})]})})]})}export{k as default};
