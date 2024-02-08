2fba41c750a3ed8fb01a2972002b2e9fe2b9f82f
Yullia90 <y.tsarenko@harwind.com.ua>
2024-02-08 22:56:37 +0200

    add validation form

--- src/components/AddColumnModal/AddColumnModal.jsx
+++ src/components/AddColumnModal/AddColumnModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import  PropTypes  from "prop-types";
 import {
   AddModalWrap,
--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,51 +1,4 @@
-// import Logo from "../../components/Logo/Logo";
-// import {
-//   WelcomeWrapper,
-//   WelcomeContainer,
-//   WelcomeText,
-//   WelcomeBtnWrapper,
-//   RegistrationBtn,
-//   LoginBtn,
-// } from "./Welcome.styled";
-
-// const size = {
-//   width: "48px",
-//   height: "48px",
-// };
-
-// const text = {
-//   color: "#161616",
-//   font: "4rem",
-// };
-
-// const icon = {
-//   bgColor: "#161616",
-//   iconColor: "#fff",
-// };
-
-// export default function Welcome() {
-//   return (
-//     <WelcomeContainer>
-//       <WelcomeWrapper>
-//         <img src="images/welcome.png" alt="boy with laptop" />
-//         <Logo size={size} text={text} icon={icon} />
-//         <WelcomeText>
-//           Supercharge your productivity and take control of your tasks with Task
-//           Pro - Don't wait, start achieving your goals now!
-//         </WelcomeText>
-//         <WelcomeBtnWrapper>
-//           <RegistrationBtn to="auth/signup">Registration</RegistrationBtn>
-//           <LoginBtn to="auth/signin">Log In</LoginBtn>
-//         </WelcomeBtnWrapper>
-//       </WelcomeWrapper>
-//     </WelcomeContainer>
-//   );
-// }
-
-
-
 import Logo from "../../components/Logo/Logo";
-import { useState } from "react";
 import {
   WelcomeWrapper,
   WelcomeContainer,
@@ -54,7 +7,6 @@ import {
   RegistrationBtn,
   LoginBtn,
 } from "./Welcome.styled";
-import AddColumnModal from "../../components/AddColumnModal/AddColumnModal";
 
 const size = {
   width: "48px",
@@ -70,15 +22,8 @@ const icon = {
   bgColor: "#161616",
   iconColor: "#fff",
 };
-const Welcome = () => {
-  // Стан для управління видимістю модального вікна
-  const [isModalColumnOpen, setIsModalColumnOpen] = useState(false);
 
-  //   // Функція для відкриття та закриття модального вікна
-   const openColumnModal = () => {
-     setIsModalColumnOpen((prevState) => !prevState);
-   };
-
+export default function Welcome() {
   return (
     <WelcomeContainer>
       <WelcomeWrapper>
@@ -91,15 +36,70 @@ const Welcome = () => {
         <WelcomeBtnWrapper>
           <RegistrationBtn to="auth/signup">Registration</RegistrationBtn>
           <LoginBtn to="auth/signin">Log In</LoginBtn>
-          <LoginBtn onClick={() => openColumnModal()}>
-            Add another column
-          </LoginBtn>
-          {isModalColumnOpen && (
-            <AddColumnModal openColumnModal={openColumnModal} />
-          )}
         </WelcomeBtnWrapper>
       </WelcomeWrapper>
     </WelcomeContainer>
   );
-};
-export default Welcome;
+}
+
+
+
+// import Logo from "../../components/Logo/Logo";
+// import { useState } from "react";
+// import {
+//   WelcomeWrapper,
+//   WelcomeContainer,
+//   WelcomeText,
+//   WelcomeBtnWrapper,
+//   RegistrationBtn,
+//   LoginBtn,
+// } from "./Welcome.styled";
+// import AddColumnModal from "../../components/AddColumnModal/AddColumnModal";
+
+// const size = {
+//   width: "48px",
+//   height: "48px",
+// };
+
+// const text = {
+//   color: "#161616",
+//   font: "4rem",
+// };
+
+// const icon = {
+//   bgColor: "#161616",
+//   iconColor: "#fff",
+// };
+// const Welcome = () => {
+//   // Стан для управління видимістю модального вікна
+//   const [isModalColumnOpen, setIsModalColumnOpen] = useState(false);
+
+//   //   // Функція для відкриття та закриття модального вікна
+//    const openColumnModal = () => {
+//      setIsModalColumnOpen((prevState) => !prevState);
+//    };
+
+//   return (
+//     <WelcomeContainer>
+//       <WelcomeWrapper>
+//         <img src="images/welcome.png" alt="boy with laptop" />
+//         <Logo size={size} text={text} icon={icon} />
+//         <WelcomeText>
+//           Supercharge your productivity and take control of your tasks with Task
+//           Pro - Don't wait, start achieving your goals now!
+//         </WelcomeText>
+//         <WelcomeBtnWrapper>
+//           <RegistrationBtn to="auth/signup">Registration</RegistrationBtn>
+//           <LoginBtn to="auth/signin">Log In</LoginBtn>
+//           <LoginBtn onClick={() => openColumnModal()}>
+//             Add another column
+//           </LoginBtn>
+//           {isModalColumnOpen && (
+//             <AddColumnModal openColumnModal={openColumnModal} />
+//           )}
+//         </WelcomeBtnWrapper>
+//       </WelcomeWrapper>
+//     </WelcomeContainer>
+//   );
+// };
+// export default Welcome;
