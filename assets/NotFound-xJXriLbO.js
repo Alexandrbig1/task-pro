cbf35e65158289ff4c168a902475827159b3e3ec
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-12 14:27:47 -0600

    Updates

--- assets/NotFound-xJXriLbO.js
+++ assets/NotFound-xJXriLbO.js
@@ -1,4 +1,4 @@
-import{u as e,p as i,j as t,H as n,a as o}from"./index-mGfAQQ9u.js";const a=e.div`
+import{u as e,p as i,j as t,H as n,a as o}from"./index-NpwfsvUE.js";const a=e.div`
   margin: 0 auto;
   width: 100%;
   min-height: 100vh;
--- assets/Welcome-8SXhBGHg.js
+++ assets/Welcome-8SXhBGHg.js
@@ -1,4 +1,4 @@
-import{u as e,p as i,L as n,j as t,H as r,a}from"./index-mGfAQQ9u.js";import{L as s}from"./Logo-HjN8GFA6.js";const l=e.div`
+import{u as e,p as i,L as n,j as t,H as r,a}from"./index-NpwfsvUE.js";import{L as s}from"./Logo-zgg4_STC.js";const l=e.div`
   background: linear-gradient(
     180deg,
     rgba(196, 196, 196, 0) 25%,
