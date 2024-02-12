d4aa321dbfd6b6f8792b8ccdb428647464798368
Alexandrbig1 <alexsmagin1@gmail.com>
2024-02-12 01:14:06 -0600

    Updates

--- assets/Welcome-oVplzodp.js
+++ assets/Welcome-oVplzodp.js
@@ -1,4 +1,4 @@
-import{u as e,p as i,L as n,j as t,H as r,a}from"./index-_MzZO4lT.js";import{L as s}from"./Logo-OzncvSoi.js";const l=e.div`
+import{u as e,p as i,L as n,j as t,H as r,a}from"./index-FgO_RX7O.js";import{L as s}from"./Logo-096rNnNa.js";const l=e.div`
   background: linear-gradient(
     180deg,
     rgba(196, 196, 196, 0) 25%,
